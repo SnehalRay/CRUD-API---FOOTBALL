481856e766ff543e6b4ba985b7056036d38446ad
SnehalRay <snehal1@ualberta.ca>
2024-06-11 17:16:38 +0400

    player model added

---
+++ models/players.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const playerSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Fill the name"],
+  },
+  position: {
+    type: String,
+    required: false,
+  },
+  nationality: {
+    type: String,
+    required: [true, "Fill the nationality"],
+    default: "N/A",
+  },
+  club: {
+    type: String,
+    required: false,
+    default: "N/A",
+  },
+  age: {
+    type: Number,
+    required: [true, "Fill the number"],
+  },
+});
+
+const Player = mongoose.model("Player",playerSchema);
+
+export default Player;
