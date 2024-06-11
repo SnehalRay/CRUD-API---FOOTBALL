ca3592af70e6019f5bbf8442a982f046aa4cb9b1
SnehalRay <snehal1@ualberta.ca>
2024-06-11 18:01:21 +0400

    Reading data of players from mongodb ids

--- index.js
+++ index.js
@@ -25,5 +25,33 @@ app.post("/api/players", async (req,res)=>{
     }
 })
 
+//GETTING DATA
+app.get("/api/players",async(req,res)=>{
+    try{
+        const player = await Player.find({})
+        res.status(200).json(player)
+    }
+    catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+//READING DATA FROM IDS
+
+app.get("/api/players/:id",async(req,res)=>{
+    try{
+        const { id } = req.params;
+        const player = await Player.findById(id);
+        if(!player){
+            res.status(500).json({messager: "Player not found" });
+        }
+        res.status(200).json(player) 
+
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
 
 app.listen(8000,()=>{console.log("Server staring...")})
